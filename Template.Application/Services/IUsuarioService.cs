1b7b814e5b1d2f2ef4204532ab3480fd5bd14347
Tobiasespin <63616161+Tobiasespin@users.noreply.github.com>
2021-05-06 12:22:17 -0300

    Add files via upload

---
+++ Template.Application/Services/IUsuarioService.cs
@@ -0,0 +1,16 @@
+﻿using Template.Domain.DTOs.Response;
+using Template.Domain.DTOs.Request;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Application.Services
+{
+    public interface IUsuarioService
+    {
+        Task<ResponseUsuarioDto> Create(RequestUsuarioDto request);
+        Task<List<ResponseUsuarioDto>> GetAll();
+    }
+}
---
+++ Template.Application/Services/UsuarioService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Template.Domain.Commands;
+using Template.Domain.DTOs.Request;
+using Template.Domain.DTOs.Response;
+using Template.Domain.Entities;
+using Template.Domain.Queries;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Application.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IGenericsRepository _repository;
+        private readonly IUsuarioQuery _query;
+        private readonly IMapper _mapper;
+
+        public UsuarioService (IGenericsRepository repository, IUsuarioQuery query, IMapper mapper)
+        {
+            _repository = repository;
+            _query = query;
+            _mapper = mapper;
+        }
+        public async Task <ResponseUsuarioDto> Create(RequestUsuarioDto request)
+        {
+            var u = _mapper.Map<Usuario>(request);
+            await _repository.Add(u);
+            return _mapper.Map<ResponseUsuarioDto>(u);
+        }
+        public async Task<List<ResponseUsuarioDto>>GetAll()
+        {
+            return await _query.GetAll();
+        }
+    }
+}
