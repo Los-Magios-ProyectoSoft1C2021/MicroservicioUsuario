540f3c2fc21e19a20a8df4465225d02df4f940f3
Tobiasespin <63616161+Tobiasespin@users.noreply.github.com>
2021-05-06 12:23:46 -0300

    Add files via upload

---
+++ Template.Domain/DTOs/Response/ResponseUsuarioDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain.DTOs.Response
+{
+    public class ResponseUsuarioDto
+    {
+        public Guid UsuarioId { get; set; }
+        public Guid Rol { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public int Dni { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Imagen { get; set; }
+        
+    }
+}
---
+++ Template.Domain/DTOs/UsuarioDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain.DTOs
+{
+    public class UsuarioDto
+    {
+        public Guid UsuarioId { get; set; }
+        public Guid Rol { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public int Dni { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Imagen { get; set; }
+    }
+}
